17c592d6141e8dc291266faf42cf1d608d9aabd9
1341mee <27hirajian@gmail.com>
2025-09-06 10:52:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from threading import Thread
+import backend_code
+Thread(target=backend_code.run).start()
 
-app = Flask(__name__)
+def run:
+    app = Flask(__name__)
+    
+    @app.route('/')
+    def hello():
+        return "Hello Back4apper!"
+    
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=8080)
 
-@app.route('/')
-def hello():
-    return "Hello Back4apper!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+run()
